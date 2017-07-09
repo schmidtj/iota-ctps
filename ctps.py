198f65acada1de36d5e2c815fb484ff3b927a139
schmidtj <jschmid1@binghamton.edu>
2017-07-09 09:24:12 -0400

    Added influxdb functionality.  Defaults in ctps.config are for localhost instance of influxdb and localhost full node.

--- ctps.py
+++ ctps.py
@@ -1,44 +1,24 @@
-"""Usage:
-  ctps.py (-e DIR | -s PORT) [-i INTERVAL] [options]
-
-
-  Options:
-      -h --help                                 show this help message and exit
-      --version                                 show version and exit
-      --testnet                                 sets Coordinator address to testnet Coo
-      -e DIR --export_folder=DIR                export folder
-      -s PORT --subscribe=PORT              subscribe to IRI zmq publications
-      -i INTERVAL --interval=INTERVAL           sampling interval [default: 30]
-      --auth_key=AUTH_KEY                       authentication key for url api endpoint
-      --url=URL                                 url api endpoint
-      --slack_key=SLACK_KEY                     slack token
-      --width                                   calculate & plot width histogram
-      --poisson                                 calculate & plot confirmation time distribution
-
-      --prune                                   prune confirmed transactions
+""" Configuration options found in ctps.config
       
 """
 
-import time
-import sys
-
-from docopt import docopt
-
+import time, sys, ConfigParser, io
 from tangle import tangle
 
 
-def main(config_map_global):
-    t = tangle(config_map_global)
+def main(config):
+    t = tangle(config)
 
-    if config_map_global["--export_folder"]:
+    if config.get("iri", 'export_folder'):
         while True:
             t.incremental_read()
             t.print_stats()
             time.sleep(t.resolution)
-    if config_map_global["--subscribe"]:
+    if int(config.get("zmq", 'use_zmq')):
         t.continuous_read()
 
 
 if __name__ == '__main__':
-    config_map_global = docopt(__doc__)
-    main(config_map_global)
+    config = ConfigParser.ConfigParser()
+    config.read("ctps.config")
+    main(config)
